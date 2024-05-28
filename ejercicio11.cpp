05bf945f02bcfe89fdda44d70899dd01e6fb73c0
GARCIA PEREZ DANNA PAOLA <168700889+DANNAPAOOO@users.noreply.github.com>
2024-05-27 18:53:42 -0600

    Update ejercicio11.cpp

--- ejercicio11.cpp
+++ ejercicio11.cpp
@@ -2,21 +2,21 @@
 #include <vector>
 
 int main() {
-  int tam; // Tamaño del vector
+  int tam; 
   std::cout << "Ingrese el tamaño del vector: ";
   std::cin >> tam;
 
-  std::vector<int> vector(tam); // Creamos un vector de tamaño tam
+  std::vector<int> vector(tam);
 
-  // Llenamos el vector con números enteros
+  
   for (int i = 0; i < tam; i++) {
     std::cout << "Ingrese el elemento " << i + 1 << ": ";
     std::cin >> vector[i];
   }
 
-  int positivos = 0; // Contador de números positivos
+  int positivos = 0;
 
-  // Contamos los números positivos
+  
   for (int i = 0; i < tam; i++) {
     if (vector[i] > 0) {
       positivos++;
